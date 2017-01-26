8e0723de30d96b0e0d6c6f8375851744c43e059a
Mumtaz Khan <mumtzkhan7@gmail.com>
2017-01-26 21:55:46 +0000

    destroy grams capability

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,14 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
+
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
@@ -41,6 +49,7 @@ class GramsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
 
   private
 
